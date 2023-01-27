4502921430997d99d6947f057e3653a99c1dd108
Edgar Mesquita <edgar.rj@gmail.com>
2023-01-27 18:39:20 +0000

    metatags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,12 +6,31 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Head from 'next/head';
 
 function MyApp({ Component, ...rest }: AppProps) {
+  const title = "Calculadora de Salário - eQuantic Tech";
+  const desc = "Cálculo de Salário para Contratos com/sem termo, Recibos Verdes ou Fatura como Empresa Unipessoal";
+  const url = "https://salary-calculator.equantic.tech";
+  const img = `${url}/logo.png`;
 
   return (
     <ThemeProvider theme={theme}>
       <Head>
-        <title>Salary Calculator</title>
+        <title>{title}</title>
+        <meta name="title" content={title} />
+        <meta name="description" content={desc} />
+
         <meta name="viewport" content="initial-scale=1, width=device-width" />
+
+        <meta property="og:url" content={url} />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={desc} />
+        <meta property="og:image" content={img} />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content={url} />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={desc} />
+        <meta property="twitter:image" content={img} />
       </Head>
       <main>
         <CssBaseline />
