6ebcfa23af4f87bdf5bd0c14524a4f77d3d698e3
Edgar Mesquita <edgar.rj@gmail.com>
2025-01-31 21:27:02 +0000

    Updates

---
+++ _next/static/_qJoOkrCGaTUHj-SDEBIM/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/578-5d384be50cd8040d.js","static/chunks/pages/index-3f2760376302e8cf.js"],"/_error":["static/chunks/pages/_error-91a854d9c9cfa29b.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
